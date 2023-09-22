d647ccda2ad98eb3c1bbe1d0210dc0234dc1aa1e
THW-Reaper <128430724+THW-Reaper@users.noreply.github.com>
2023-09-22 11:28:22 -0500

    convert installer to header

--- PavlovMapsModioInstaller.cpp
+++ PavlovMapsModioInstaller.cpp
@@ -1,18 +1,19 @@
-// Goals
+/* Goals
 
-// Installer
-// []Check if dotnet is installed 
-// []install main program from releases page to desired location
-// []install updater/installer to the same directory/folder
-// []self delete
+   Installer
+ []Check if dotnet is installed 
+ []install main program from releases page to desired location
+ []install updater/installer to the same directory/folder
+ []self delete
 
-// Updater
-// []Check either on startup of main program or startup of computer
-// []Check exe version and compare to github release version, tbd how, thinking download latest exe and check if the version is greater
-// []get directory of self
+   Updater
+ []Check either on startup of main program or startup of computer
+ []Check exe version and compare to github release version, tbd how, thinking download latest exe and check if the version is greater
+ []get directory of self
 
-// Overall
-// comment on everything to document decently
+  Overall
+ []comment on everything to document decently
+ []Learn how to properly use/make headers
 
 #include <iostream> //Basic input/output
 #include <sys/stat.h> //file stuff iirc
@@ -22,18 +23,12 @@
 #include <direct.h>
 #include <chrono> //delay stuff
 #include <thread> 
-#pragma comment(lib, "urlmon.lib") //Need to remember what this does
+#pragma comment(lib, "urlmon.lib") //Need to remember what this does */
+#include "Installer.hpp"
 using namespace std; // for ticking off veteran c++ people who think using namespace std is weird or something 
 
-void Install(); //Install funtion
-{
-
-}
-void Update(); //Update Func
-{
-
-}
 int main();
 {
-
+bool installed false;
+installed.exists() 
 }
