1e91b2f808a86a46fc6f6f49fb820549cb469252
THW-Reaper <128430724+THW-Reaper@users.noreply.github.com>
2023-10-19 13:21:20 -0500

    no more headers

--- PavlovMapsModioInstaller.cpp
+++ PavlovMapsModioInstaller.cpp
@@ -26,8 +26,14 @@
 #include <sys/stat.h> //file stuff, used in our case for checking if dotnet is installed
 #include <iostream> //Basic input/output
 #include <filesystem>
-#include "InstallerAndupdater.hpp"
-using namespace std; 
+#include <stdlib.h>
+#include <string>//strings 
+#include <Windows.h>//system commands
+#include <direct.h>
+#include <chrono> //Time
+#include <thread> //Threads
+#pragma comment(lib, "urlmon.lib")
+using namespace std;
 void StartInstall();
 
 int main()
@@ -73,4 +79,69 @@ void StartInstall ()
         
  
     return;
+}
+
+int Install(string installdir)
+{
+        //sets command line strings, Way more vars than necessary
+        installdir.assign(installdir + "\\PavlovModioDownloader");
+        string createinstalldircmd = "md " + installdir;//Creates a folder at the specified directory 
+        string rawurl = " https://github.com/RainOrigami/DownloadPavlovMapsFromModIo/releases/latest/download/DownloadPavlovMapsFromModIo.exe";
+        string updaterrawurl = " https://github.com/THW-Reaper/DownLoadPavlovMapsFromModio_Installer_and_Updater/releases/latest/download/Installer.exe";
+        string installdir_and_url = installdir + rawurl;
+        string Updaterinstalldir_and_url = installdir + updaterrawurl;
+        string installdircmd = "curl -O --output-dir " + installdir_and_url;
+        string installupdatercmd = "curl -o --output-dir Updater.exe " + Updaterinstalldir_and_url;
+        //commands
+        system(createinstalldircmd.c_str());
+        cout << "File Path Created, downloading file";
+        system(installdircmd.c_str());
+        system(installupdatercmd.c_str());
+        std::this_thread::sleep_for(5s);
+        system("start DownloadPavlovMapsFromModIo.exe"); //Boots the installed exe
+        cout << "Download Successful!"<<endl;
+        std::this_thread::sleep_for(10s);
+        exit(0); //exits program after delay
+}
+
+void dotnet()
+{     
+        for (bool valid = false; valid = false;)
+            {
+                char installQuery;
+                cout << "Dotnet 6 Framwork not found, would you like to install it?[Y/N]";
+                cin >> installQuery;
+                if (installQuery == 'y')
+                {
+                    system("winget install Microsoft.DotNet.DesktopRuntime.6");
+                    valid = true;
+                }
+                else if (installQuery == 'Y')
+                {
+                    system("winget install Microsoft.DotNet.DesktopRuntime.6");
+                    valid = true;
+                }
+                else if (installQuery == 'n')
+                {
+                    cout << "Terminating program... I'll be back!";
+                    sleep_for(5s);
+                    exit(0);
+                }
+                else if (installQuery == 'N')
+                {
+                    cout << "Terminating program... I'll be back!";
+                    sleep_for(5s);
+                    exit(0);
+                }
+                else
+                {
+                    cout << "Invalid Command!";
+                }
+            }
+}
+
+void Update()
+{   //Since the program and updater are at the same directory, the program should already be cd'd to the same folder as the program
+    system("curl -o https://github.com/RainOrigami/DownloadPavlovMapsFromModIo/releases/latest/download/DownloadPavlovMapsFromModIo.exe");
+    std::this_thread::sleep_for(5s);
 }
